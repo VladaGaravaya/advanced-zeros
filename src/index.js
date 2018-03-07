42e329295b2408835f041a949fbea7b2406bfa0a
VladaGaravaya <36517813+VladaGaravaya@users.noreply.github.com>
2018-03-07 18:51:46 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  var remain=1;
+    var i=1, zeros=0;
+    while(remain>=1){
+        remain=Math.trunc(number/(Math.pow(5,i)));
+        i++;
+        zeros=zeros+remain;
+    }
+    return zeros;
 }
